243e700049a66259b04553138305dbc1fb1ee340
moha1313128 <moha1313128@gmail.com>
2019-12-07 18:50:57 +0100

    Modifing Home Page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,36 +15,78 @@
 </head>
 <body>
     <div id="app">
-        <nav class="nav has-shadow">
-            <div class="container">
-                <div class="nav-left">
-                    <a class="nav-item" href="{{route('home')}}">
-                        <img src="{{asset('images/devmarketer-logo.png')}}" alt="logo">        
-                    </a>
-                    <a href="#" class="nav-item is-tab is-hidden-mobile m-l-10">Learn</a>
-                    <a href="#" class="nav-item is-tab is-hidden-mobile">Discuss</a>
-                    <a href="#" class="nav-item is-tab is-hidden-mobile">Share</a>
-                </div>
-                <div class="nav-right" style="overflow: visible;">
-                    @if (!Auth::guest())
-                        <a href="#" class="nav-item is-tab">Login</a>
-                        <a href="#" class="nav-item is-tab">Join Us</a>
-                    @else 
-                        <button class="dropdown is-aligned-right nav-item is-tab">
-                            Hey there <span class="icon"><i class="fa fa-caret-down"></i></span>
+        <div class="container">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item" >
+              <img src="{{asset('images/devmarketer-logo.png')}}"  width="112" height="100">
+            </a>
 
-                            <ul class="dropdown-menu">
-                                <li><a href="#">Profile</a></li>
-                                <li><a href="#">Noifications</a></li>
-                                <li><a href="#">Settings</a></li>
-                                <li class="seperator"></li>
-                                <li><a href="#">Logout</a></li>
-                            </ul>
-                        </button>
-                    @endif
+            <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+              <span aria-hidden="true"></span>
+            </a>
+          </div>
+
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item">
+                Learn
+              </a>
+
+              <a class="navbar-item">
+                Discuss
+              </a>
+
+              <a class="navbar-item">
+                Share
+              </a>
+
+              <div class="navbar-item has-dropdown is-hoverable" style="overflow: visible;">
+                <a class="navbar-link">
+                  Hey There
+                </a>
+
+                <div class="navbar-dropdown">
+                  <a class="navbar-item">
+                    Profile
+                  </a>
+                  <a class="navbar-item">
+                    Notifications
+                  </a>
+                  <a class="navbar-item">
+                    Settings
+                  </a>
+                  <hr class="navbar-divider">
+                  <a class="navbar-item">
+                    Logout
+                  </a>
                 </div>
+              </div>
             </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <div class="buttons">
+                @if (!Auth::guest())
+                  <a class="button is-primary">
+                    <strong>Sign up</strong>
+                  </a>  
+                  <a class="button is-light">
+                    Log in
+                  </a>
+                @else
+                  <a class="button is-primary">
+                      <strong>Log out</strong>
+                  </a>
+                @endif  
+                </div>
+              </div>
+            </div>
+          </div>
         </nav>
+        </div>
         @yield('content')
     </div>
     <!-- Scripts -->
